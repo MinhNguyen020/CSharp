9c3dc4194fef874f1dcd938616c0c1f6c07551ae
MinhNguyen020 <148432626+MinhNguyen020@users.noreply.github.com>
2023-11-03 15:21:56 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" nhap ten cua ban :");
+            string ten = Console.ReadLine();
+            Console.Write(" nhap tuoi cua ban: ");
+            int tuoi = Int16.Parse(Console.ReadLine());
+            Console.Write(" quen cua ban: ");
+            string que = Console.ReadLine();
+            Console.WriteLine(" ban ten la: {0},{1} tuoi, que o {2}" ,ten, tuoi, que);
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
